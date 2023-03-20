974d8dea6b1bddddd4aa13d046e4dd785615ab55
Areeb Khan <55325380+areebkhan274427@users.noreply.github.com>
2023-03-21 00:46:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
+// function calculateMinCost() {
+//   //your code here
+// 	var input=document.getElementById("rope-lengths").value;
+
+// 	var inputarr=input.split(",");
+	
+// 	for(let i=0;i<inputarr.length;i++){
+// 		inputarr[i]=Number(inputarr[i]);
+// 	}
+	
+	
+
+// 	let cost=0;
+	
+// 	while(inputarr.length>1){
+// 		inputarr.sort(function(a,b) {return a-b;});	
+// 		let val=inputarr[0]+inputarr[1];
+// 		 cost+=val;
+// 		inputarr.splice(0,2);
+// 		inputarr.push(val);	
+// 	}
+
+// 	document.getElementById("result").innerHTML=cost;
+  
+  
+// }  
+
 function calculateMinCost() {
   //your code here
-	var input=document.getElementById("rope-lengths").value;
 
-	var inputarr=input.split(",");
-	
-	for(let i=0;i<inputarr.length;i++){
-		inputarr[i]=Number(inputarr[i]);
-	}
-	
-	
+	 let str = document.querySelector("#rope-lengths").value;
+    let arr = str.split(", ");
 
-	let cost=0;
-	
-	while(inputarr.length>1){
-		inputarr.sort(function(a,b) {return a-b;});	
-		let val=inputarr[0]+inputarr[1];
-		 cost+=val;
-		inputarr.splice(0,2);
-		inputarr.push(val);	
-	}
+    for(let i in arr){
+        arr[i] =  Number(arr[i]);
+    }
+     let mincost=0;
+    while(arr.length!=1){
+        arr.sort( function (a,b){
+            return a-b;
+        });
+        let cost= arr[0] + arr[1];
+        mincost+=cost;
+        arr.splice(0,2);
+        arr.push(cost);
+    }
 
-	document.getElementById("result").innerHTML=cost;
-  
-  
-}  
+
+    document.querySelector("#result").innerHTML=mincost;
+  
+}
