75eb19e6fae73dafe96686d639838d5761267dd0
Areeb Khan <55325380+areebkhan274427@users.noreply.github.com>
2023-03-20 20:36:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 function calculateMinCost() {
   //your code here
 	var input=document.querySelector("#rope-lengths").value;
+
 	var inputarr=input.split(",");
 	for(let i=0;i<inputarr.length;i++){
 		inputarr[i]=Number(inputarr[i]);
 	}
 	
-
-	inputarr.sort(function(a,b) {return a-b;});
 	
 
 	let cost=0;
 	
 	while(inputarr.length>1){
-		let val=inputarr[0]+inputarr[1];
-		let cost+=val;
-		inputarr.splice(0,2);
-		input.push(val);
-		alert(val);
 		inputarr.sort(function(a,b) {return a-b;});	
+		let val=inputarr[0]+inputarr[1];
+		 cost+=val;
+		inputarr.splice(0,2);
+		inputarr.push(val);	
 	}
 
 	document.querySelector("#result").textContent=cost;
